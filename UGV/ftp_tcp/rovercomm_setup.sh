e6f8759f31efaec9c7848d72266cf67f081650f7
Antony Yung <22206795+antonyctyung@users.noreply.github.com>
2022-03-23 16:05:51 -0700

    added priviledge check for setup script

--- UGV/ftp_tcp/rovercomm_setup.sh
+++ UGV/ftp_tcp/rovercomm_setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 apt install -y vsftpd
 printf 'listen=NO\nlisten_ipv6=YES\nanonymous_enable=NO\nlocal_enable=YES\nwrite_enable=YES\nlocal_umask=022\ndirmessage_enable=YES\nuse_localtime=YES\nxferlog_enable=YES\nconnect_from_port_20=YES\nchroot_local_user=YES\nsecure_chroot_dir=/var/run/vsftpd/empty\npam_service_name=ftp\nrsa_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem\nrsa_private_key_file=/etc/ssl/private/ssl-cert-snakeoil.key\nssl_enable=NO\n' > /etc/vsftpd.conf
 service vsftpd restart
